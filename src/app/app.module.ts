24a2814a05de975496bae58e00c79313395b1fb7
vikram <vvikky8562@gmail.com>
2018-09-18 18:40:51 +0530

    Removed Unwanted Code
    Removed Unwanted Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,11 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { CompareValidatorDirective } from './compare-validator.directive';
-import { ShowDomDirective } from './show-dom.directive';
-import { PasswordCheckDirective } from './password-check.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompareValidatorDirective,
-    ShowDomDirective,
-    PasswordCheckDirective
+    CompareValidatorDirective
   ],
   imports: [
     BrowserModule,
