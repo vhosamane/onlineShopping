7de1b8b0681f7016add63462c0039e840ac9d163
vikram <vvikky8562@gmail.com>
2018-09-18 20:53:46 +0530

    Removed https URL
    Removed https URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
   production: false,
-  apiUrl: 'https://shoppingcart-5e394.firebaseio.com/',
-  apiKey: "AIzaSyBJ4b0Bf93ft7573fH36M5lf429ah23Ybk",
-  authDomain: "shoppingcart-5e394.firebaseapp.com",
-  databaseURL: "https://shoppingcart-5e394.firebaseio.com",
-  projectId: "shoppingcart-5e394",
-  storageBucket: "shoppingcart-5e394.appspot.com",
-  messagingSenderId: "539126118460"
+  apiUrl: 'api url',
+  apiKey: "api key",
+  authDomain: "",
+  databaseURL: "database url",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+.
